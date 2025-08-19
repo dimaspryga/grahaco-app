def2fbab508779073a96521aa50af3b82a9b552c
dimaspryga <dimaspryga9@gmail.com>
2025-08-19 09:08:47 +0700

    feat : fix title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,32 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { Manrope } from "next/font/google"
-import "./globals.css"
+import type React from "react";
+import type { Metadata } from "next";
+import { Manrope } from "next/font/google";
+import "./globals.css";
 
 const manrope = Manrope({
   subsets: ["latin"],
   display: "swap",
   variable: "--font-manrope",
   weight: ["400", "500", "700"],
-})
+});
 
 export const metadata: Metadata = {
-  title: "Grahaco - Arsitektur & Kontraktor Profesional",
-  description: "Layanan arsitektur, kontraktor, desain, dan kelola properti profesional di Indonesia",
+  title: "Grahaco - Perusahaan Pengelola Properti Profesional",
+  description:
+    "Layanan arsitektur, kontraktor, desain, dan kelola properti profesional di Indonesia",
   generator: "v0.app",
-}
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="id" className={`${manrope.variable} antialiased`}>
-      <body className="min-h-screen bg-background font-sans text-foreground">{children}</body>
+      <body className="min-h-screen bg-background font-sans text-foreground">
+        {children}
+      </body>
     </html>
-  )
+  );
 }
